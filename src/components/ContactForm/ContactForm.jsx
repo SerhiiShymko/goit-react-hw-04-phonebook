0875cc8e5361772ccc4f60e9dfb583597035f0d1
SerhiiShymko <shimko456@gmail.com>
2023-05-16 16:52:45 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,62 @@
 import PropTypes from 'prop-types';
 import css from './ContactForm.module.css';
+// import { useState } from 'react';
 import { Component } from 'react';
 
+// export default function ContactForm(onSubmit) {
+//   const [name, setName] = useState('');
+//   const [ number, setNumber ] = useState('');
+
+//   const handleChange = e => {
+//     // const { name, value } = e.currentTarget;
+//     setName(value);
+//   };
+
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//     const { onSubmit } = this.props;
+//     onSubmit(this.state);
+//     this.reset();
+//   };
+
+//   reset = () => {
+//     this.setState({ name: '', number: '' });
+//   };
+
+//   return (
+//     <form className={css.form} onSubmit={handleSubmit}>
+//       <label className={css.label}>
+//         <span className={css.title}>Name</span>
+//         <input
+//           className={css.input}
+//           onChange={this.handleChange}
+//           type="text"
+//           name="name"
+//           value={name}
+//           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//           required
+//         />
+//       </label>
+//       <label className={css.label}>
+//         <span className={css.title}>Number</span>
+//         <input
+//           className={css.input}
+//           onChange={this.handleChange}
+//           type="tel"
+//           name="number"
+//           value={number}
+//           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//           required
+//         />
+//       </label>
+//       <button className={css.button} type="submit">
+//         Add contact
+//       </button>
+//     </form>
+//   );
+// }
 
 class ContactForm extends Component {
   static propTypes = {
@@ -68,4 +123,7 @@ class ContactForm extends Component {
   }
 }
 
+// ContactForm.prototype = {
+//   onSubmit: PropTypes.func.isRequired,
+// };
 export default ContactForm;
